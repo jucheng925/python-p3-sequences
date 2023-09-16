a1de5a22cc6c847716959fc02af079669e809e8f
Julie C <jucheng925@gmail.com>
2023-09-16 17:22:27 -0400

    working solution to print a fibonacci list based on the lenght pass through

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = []
+    for i in range(length):
+        if i == 0:
+            my_list.append(0)
+        elif i == 1:
+            my_list.append(1)
+        else:
+            sum = my_list[i-2] + my_list[i-1]
+            my_list.append(sum)
+    print(my_list)
